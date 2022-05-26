8ac3185bb5db377724766ddcb4e4abc3b36445aa
Shyam70233 <deluxshop468@gmail.com>
2022-05-26 13:21:22 +0530

    state with add ore example

--- src/App.js
+++ src/App.js
@@ -2,18 +2,55 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  const [topics, settopics] = useState([]);
-  const addClick = () => {
-    const mytopics = prompt('Enter Your Topics', 'Topics......');
-    settopics([...topics, mytopics]);
+  const initialState = {
+    item: '',
+    qty: 0,
+    price: 0,
   };
+  const [stock, setstock] = useState([initialState]);
+  const addmoreitem = () => {
+    setstock([...stock, initialState]);
+  };
+  const handleChange = (i, name, e) => {
+    let value = e.target.value;
+    setstock(
+      stock?.map((x, index) => (index === i ? { ...x, [name]: value } : x))
+    );
+  };
+
+  const totalPrice = stock
+    .map((x) => +x.price)
+    .reduce((sum, next) => sum + next);
+
+  const totalQty = stock.map((x) => +x.qty).reduce((sum, next) => sum + next);
   return (
     <div>
-      <button onClick={addClick}>Click Me</button>
-      <h1>Hooks : {topics?.length} </h1>
-      {topics?.map((item) => (
-        <li>{item}</li>
+      <button onClick={addmoreitem}>Add More</button>
+      {stock?.map((itemss, index) => (
+        <div className="stock-item">
+          <h2>Items : {index} </h2>
+          <input
+            onChange={(e) => handleChange(index, 'item', e)}
+            value={itemss.item}
+            placeholder="Enter item"
+          />
+          <input
+            onChange={(e) => handleChange(index, 'qty', e)}
+            value={itemss.qty}
+            placeholder="Enter qty"
+          />
+          <input
+            onChange={(e) => handleChange(index, 'price', e)}
+            value={itemss.price}
+            placeholder="Enter price"
+          />
+        </div>
       ))}
+      <h1>
+        Totaltem : {stock.length}
+        totalPrice : {totalPrice}
+        totalQty : {totalQty}
+      </h1>
     </div>
   );
 }
